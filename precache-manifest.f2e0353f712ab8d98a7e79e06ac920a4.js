40a6ef8005bba24b6862e27957955f4dacce6c76
letgodoy <let.godoy@live.com>
2019-04-08 21:38:27 -0300

    Updates

---
+++ precache-manifest.f2e0353f712ab8d98a7e79e06ac920a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rankingnpm%20run%20deploy/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "052629c6e8c1f4192eec",
+    "url": "/rankingnpm%20run%20deploy/static/css/main.4eee8cd2.chunk.css"
+  },
+  {
+    "revision": "88e539e39fa10d4a599c",
+    "url": "/rankingnpm%20run%20deploy/static/js/runtime~main.c0d671a0.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rankingnpm%20run%20deploy/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4af527f794004bf34037",
+    "url": "/rankingnpm%20run%20deploy/static/js/2.4e3794c8.chunk.js"
+  },
+  {
+    "revision": "052629c6e8c1f4192eec",
+    "url": "/rankingnpm%20run%20deploy/static/js/main.8dcac5e8.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rankingnpm%20run%20deploy/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rankingnpm%20run%20deploy/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rankingnpm%20run%20deploy/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "4af527f794004bf34037",
+    "url": "/rankingnpm%20run%20deploy/static/css/2.9fd97d00.chunk.css"
+  },
+  {
+    "revision": "02a3b7b0a26d7ca297919085554d272f",
+    "url": "/rankingnpm%20run%20deploy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ranking/precache-manifest.536eed58389ffb96132726692d69ea38.js"
+  "/rankingnpm%20run%20deploy/precache-manifest.f2e0353f712ab8d98a7e79e06ac920a4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ranking/index.html", {
+workbox.routing.registerNavigationRoute("/rankingnpm%20run%20deploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
